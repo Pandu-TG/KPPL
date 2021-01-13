8a46f452324d063f52c53880a02593229c6bdd7a
Matahari Kridha <matakridha@users.noreply.github.com>
2021-01-13 07:52:59 +0700

    Update

--- PANDU/app/src/main/java/com/stigma15/pandu/Activity/LoginActivity.kt
+++ PANDU/app/src/main/java/com/stigma15/pandu/Activity/LoginActivity.kt
@@ -1,13 +1,17 @@
 package com.stigma15.pandu.Activity
 
 import android.app.Activity
+import android.app.ProgressDialog
 import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.stigma15.pandu.R
 import kotlinx.android.synthetic.main.activity_login2.*
 
@@ -16,9 +20,31 @@ class LoginActivity : AppCompatActivity(){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login2)
 
-        logins_button.setOnClickListener { val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
-            finish()}
+        logins_button.setOnClickListener {
+            val email = inputEmail.text.toString()
+            val password = inputPassword.text.toString()
+            if (email.isEmpty()|| password.isEmpty()) {
+                Toast.makeText(this, "Please Insert Email and Password", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener{
+
+                    if (!it.isSuccessful){ return@addOnCompleteListener
+                        val intent = Intent (this, MainActivity::class.java)
+                        startActivity(intent)
+                    }
+                    else
+                        Toast.makeText(this, "Succesfully Login", Toast.LENGTH_SHORT).show()
+                    val intent = Intent (this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+                .addOnFailureListener{
+                    Log.d("Main", "Failed Login: ${it.message}")
+                    Toast.makeText(this, "Email/Password incorrect", Toast.LENGTH_SHORT).show()
+            }
+        }
+
         signUp.setOnClickListener { val intent = Intent(this, SignUpActivity::class.java)
             startActivity(intent)
             finish()}
